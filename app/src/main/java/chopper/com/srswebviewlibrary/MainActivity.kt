0fd82eb2329165f642728540626da6f4eabbedf0
renxiao <james.ren@yo-ren.com>
2018-08-25 14:18:56 +0800

    first commit

---
+++ app/src/main/java/chopper/com/srswebviewlibrary/MainActivity.kt
@@ -0,0 +1,12 @@
+package chopper.com.srswebviewlibrary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
